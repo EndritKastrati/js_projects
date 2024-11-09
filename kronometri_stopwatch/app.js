0c51c9d574a6634139caa2d1a3f9c6df5ff7d838
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-09 20:47:02 +0100

    Linking querySelectors

---
+++ kronometri_stopwatch/app.js
@@ -0,0 +1,5 @@
+const kohaShfaqu = document.querySelector("#kohaShfaqu");
+const startoBtn = document.querySelector("#startoBtn");
+const pauzoBtn = document.querySelector("#pauzoBtn");
+const ristartoBtn = document.querySelector("#ristartoBtn");
+
