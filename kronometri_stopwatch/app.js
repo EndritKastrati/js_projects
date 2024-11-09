817acb7241a9e42163fccccc004a8e38dff2d16e
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-09 20:47:36 +0100

    Adding the needed variables

--- kronometri_stopwatch/app.js
+++ kronometri_stopwatch/app.js
@@ -3,3 +3,11 @@ const startoBtn = document.querySelector("#startoBtn");
 const pauzoBtn = document.querySelector("#pauzoBtn");
 const ristartoBtn = document.querySelector("#ristartoBtn");
 
+let kohaFillo = 0;
+let kohaKaluar = 0;
+let kohaAktuale = 0;
+let pauzuar = true;
+let intervaliId;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
